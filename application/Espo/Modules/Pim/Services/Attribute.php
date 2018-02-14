35e54d88de8248305eea2f52296b17547d636cc1
r.ratsun <r.ratsun@zinitsolutions.com>
2018-02-14 13:53:49 +0200

    59146

--- application/Espo/Modules/Pim/Services/Attribute.php
+++ application/Espo/Modules/Pim/Services/Attribute.php
@@ -21,37 +21,50 @@ class Attribute extends AbstractService
         // prepare result
         $result = [];
 
+        // get multilang fields
+        $multilangFields = $this->getMultilangFields();
+
         // get all attributes
         $attributes = $this->getEntityManager()->getRepository('Attribute')->find();
 
         if (count($attributes) > 0) {
             // prepare no family data
             $noFamilyData = [
-                'id'   => 'no_family',
-                'name' => 'No family',
+                'id'   => 'all',
+                'name' => $this->getTranslate('All', 'filterLabels', 'Attribute'),
                 'rows' => []
             ];
 
             foreach ($attributes as $attribute) {
-                $families = $attribute->get('productFamilyAttributes');
-                if (count($families) > 0) {
-                    foreach ($families as $family) {
-                        $result[$family->get('productFamilyId')]['id']     = $family->get('productFamilyId');
-                        $result[$family->get('productFamilyId')]['name']   = $family->get('productFamilyName');
-                        $result[$family->get('productFamilyId')]['rows'][] = [
-                            'attributeId' => $attribute->get('id'),
-                            'name'        => $attribute->get('name'),
-                            'type'        => $attribute->get('type')
-                        ];
+                if (!in_array($attribute->get('type'), $multilangFields)) {
+                    // get families
+                    $families = $attribute->get('productFamilyAttributes');
+
+                    // push items to families
+                    if (count($families) > 0) {
+                        foreach ($families as $family) {
+                            if (!$family->get('productFamily')->get('deleted')) {
+                                $result[$family->get('productFamilyId')]['id']     = $family->get('productFamilyId');
+                                $result[$family->get('productFamilyId')]['name']   = $family->get('productFamilyName');
+                                $result[$family->get('productFamilyId')]['rows'][] = [
+                                    'attributeId' => $attribute->get('id'),
+                                    'name'        => $attribute->get('name'),
+                                    'type'        => $attribute->get('type')
+                                ];
+                            }
+                        }
                     }
-                } else {
-                    $noFamilyData['rows'][] = [
+
+                    // push to all
+                    $noFamilyData['rows'][$attribute->get('id')] = [
                         'attributeId' => $attribute->get('id'),
                         'name'        => $attribute->get('name'),
                         'type'        => $attribute->get('type')
                     ];
                 }
             }
+            $noFamilyData['rows'] = array_values($noFamilyData['rows']);
+
             // prepare result
             $result = array_values($result);
 
@@ -62,5 +75,18 @@ class Attribute extends AbstractService
         }
 
         return $result;
+    }
+
+    /**
+     * Get multilang fields
+     *
+     * @return array
+     */
+    protected function getMultilangFields(): array
+    {
+        // get config
+        $config = $this->getConfig()->get('modules');
+
+        return (!empty($config['multilangFields'])) ? array_keys($config['multilangFields']) : [];
     }
 }
