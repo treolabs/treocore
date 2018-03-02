52b2d21bd1a54134b7c9c577ae2d0063a64deeab
r.ratsun <r.ratsun@zinitsolutions.com>
2018-03-02 08:57:25 +0200

    59764

--- api/v1/index.php
+++ api/v1/index.php
@@ -1,5 +1,5 @@
 <?php
-/************************************************************************
+/**
  * This file is part of EspoCRM and/or TreoPIM.
  *
  * EspoCRM - Open Source CRM application.
@@ -31,7 +31,7 @@
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "EspoCRM" word
  * and "TreoPIM" word.
- ************************************************************************/
+ */
 
 require_once('../../bootstrap.php');
 
