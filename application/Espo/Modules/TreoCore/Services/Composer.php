5aeda90afaa79acb39c8e94e1e1a64829d26f3af
r.ratsun <r.ratsun@zinitsolutions.com>
2018-06-22 09:35:06 +0300

    65525

--- application/Espo/Modules/TreoCore/Services/Composer.php
+++ application/Espo/Modules/TreoCore/Services/Composer.php
@@ -108,7 +108,7 @@ class Composer extends Base
         $composer = $this->run('update');
 
         // rebuild
-        $this->getInjection('dataManager')->rebuild();
+        $this->rebuild();
 
         // triggered after action
         $composer = $eventManager
@@ -272,7 +272,7 @@ class Composer extends Base
     public function setModuleComposerJson(array $data): void
     {
         $file = fopen($this->moduleComposer, "w");
-        fwrite($file, Json::encode($data, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES));
+        fwrite($file, Json::encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
         fclose($file);
     }
 
