928e33be37aaaad255bda7f6ae7597e9e28f219a
r.ratsun <r.ratsun@zinitsolutions.com>
2018-09-04 11:57:58 +0300

    create console command for rest api generating

--- application/Espo/Modules/TreoCore/Console/GenerateApidocs.php
+++ application/Espo/Modules/TreoCore/Console/GenerateApidocs.php
@@ -34,17 +34,42 @@
 
 declare(strict_types=1);
 
-namespace Espo\Modules\TreoCore\Configs;
+namespace Espo\Modules\TreoCore\Console;
 
-use Espo\Modules\TreoCore\Console;
+/**
+ * Class GenerateApidocs
+ *
+ * @author r.ratsun r.ratsun@zinitsolutions.com
+ */
+class GenerateApidocs extends AbstractConsole
+{
+    /**
+     * Get console command description
+     *
+     * @return string
+     */
+    public static function getDescription(): string
+    {
+        return 'Generate REST API documentation.';
+    }
 
-return [
-    "list"                         => Console\ListCommand::class,
-    "clear cache"                  => Console\ClearCache::class,
-    "rebuild"                      => Console\Rebuild::class,
-    "cron"                         => Console\Cron::class,
-    "events"                       => Console\Events::class,
-    "migrate <module> <from> <to>" => Console\Migrate::class,
-    "composer <command>"           => Console\Composer::class,
-    "generate apidocs"             => Console\GenerateApidocs::class
-];
+    /**
+     * Run action
+     *
+     * @param array $data
+     */
+    public function run(array $data): void
+    {
+        // generate
+        $result = $this
+            ->getContainer()
+            ->get('serviceFactory')
+            ->create('RestApiDocs')
+            ->generateDocumentation();
+        if (!empty($result)) {
+            self::show('REST API documentation generated successfully', self::SUCCESS);
+        } else {
+            self::show('Something wrong. REST API documentation generated failed. Check log for details', self::ERROR);
+        }
+    }
+}
