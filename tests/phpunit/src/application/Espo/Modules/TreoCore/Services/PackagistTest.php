2828a3d8ea871df0d09815a0f456536d48c049dd
r.ratsun <r.ratsun@zinitsolutions.com>
2018-09-13 17:43:49 +0300

    68884

--- tests/phpunit/src/application/Espo/Modules/TreoCore/Services/PackagistTest.php
+++ tests/phpunit/src/application/Espo/Modules/TreoCore/Services/PackagistTest.php
@@ -35,7 +35,7 @@ declare(strict_types=1);
 
 namespace Espo\Modules\TreoCore\Services;
 
-use PHPUnit\Framework\TestCase;
+use TreoPHPUnit\Framework\TestCase;
 
 /**
  * Class PackagistTest
@@ -51,19 +51,10 @@ class PackagistTest extends TestCase
 
     protected function setUp()
     {
-        define('CORE_PATH', '/var/www/treocore');
-        $this->self = new Packagist();
-//        $this->objects['serviceFactory'] = $this->getMockBuilder('\Espo\Core\ServiceFactory')->disableOriginalConstructor()->getMock();
-//        $this->objects['config'] = $this->getMockBuilder('\Espo\Core\Utils\Config')->disableOriginalConstructor()->getMock();
-//        $this->objects['fileManager'] = $this->getMockBuilder('\Espo\Core\Utils\File\Manager')->disableOriginalConstructor()->getMock();
-//        $this->objects['metadata'] = $this->getMockBuilder('\Espo\Core\Utils\Metadata')->disableOriginalConstructor()->getMock();
-//        $this->objects['acl'] = $this->getMockBuilder('\Espo\Core\Acl')->disableOriginalConstructor()->getMock();
-//        $this->importService = new \Espo\Services\Import();
-//        $this->importService->inject('serviceFactory', $this->objects['serviceFactory']);
-//        $this->importService->inject('config', $this->objects['config']);
-//        $this->importService->inject('fileManager', $this->objects['fileManager']);
-//        $this->importService->inject('metadata', $this->objects['metadata']);
-//        $this->importService->inject('acl', $this->objects['acl']);
+        $this->self = $this->getMockBuilder(Packagist::class)
+//            ->setMethods(['getConfig'])
+//            ->setMethodsExcept(['refresh'])
+            ->getMock();
     }
 
     /**
