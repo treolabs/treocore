f22b79aae26f8e769e09a4271ad570aa39518bd6
r.ratsun <r.ratsun@zinitsolutions.com>
2018-09-19 12:18:18 +0300

    68952

--- application/Treo/Services/MysqlCharacter.php
+++ application/Treo/Services/MysqlCharacter.php
@@ -32,28 +32,46 @@
  * and "TreoPIM" word.
  */
 
-namespace Espo\Services;
+declare(strict_types=1);
 
-class MysqlCharacter extends \Espo\Core\Services\Base
+namespace Treo\Services;
+
+use Espo\Core\Utils\Database\Schema\Utils;
+
+/**
+ * MysqlCharacter service
+ *
+ * @author r.ratsun r.ratsun@zinitsolutions.com
+ */
+class MysqlCharacter extends \Espo\Services\MysqlCharacter
 {
+
+    /**
+     * @inheritdoc
+     */
     protected function init()
     {
-        $this->addDependency('container');
+        $this->addDependencyList(
+            [
+                'entityManager',
+                'ormMetadata',
+                'schema',
+                'config',
+                'database',
+                'dataManager'
+            ]
+        );
     }
 
-    protected function getContainer()
-    {
-        return $this->getInjection('container');
-    }
-
+    /**
+     * @inheritdoc
+     */
     public function jobConvertToMb4()
     {
-        $container = $this->getContainer();
+        $pdo = $this->getInjection('entityManager')->getPDO();
+        $ormMeta = $this->getInjection('ormMetadata')->getData(true);
 
-        $pdo = $container->get('entityManager')->getPDO();
-        $ormMeta = $container->get('ormMetadata')->getData(true);
-
-        $databaseSchema = $container->get('schema');
+        $databaseSchema = $this->getInjection('schema');
         $maxIndexLength = $databaseSchema->getMaxIndexLength();
         if ($maxIndexLength > 1000) {
             $maxIndexLength = 1000;
@@ -68,14 +86,14 @@ class MysqlCharacter extends \Espo\Core\Services\Base
             $sth->execute();
         }
 
-        $fieldListExceededIndexMaxLength = \Espo\Core\Utils\Database\Schema\Utils::getFieldListExceededIndexMaxLength($ormMeta, $maxIndexLength);
+        $fieldListExceededIndexMaxLength = Utils::getFieldListExceededIndexMaxLength($ormMeta, $maxIndexLength);
 
         foreach ($ormMeta as $entityName => $entityParams) {
 
             $tableName = \Espo\Core\Utils\Util::toUnderScore($entityName);
 
             //Get table columns params
-            $query = "SHOW FULL COLUMNS FROM `". $tableName ."` WHERE `Collation` <> 'utf8mb4_unicode_ci'";
+            $query = "SHOW FULL COLUMNS FROM `" . $tableName . "` WHERE `Collation` <> 'utf8mb4_unicode_ci'";
 
             try {
                 $sth = $pdo->prepare($query);
@@ -88,10 +106,9 @@ class MysqlCharacter extends \Espo\Core\Services\Base
             $columnParams = array();
             $rowList = $sth->fetchAll(\PDO::FETCH_ASSOC);
             foreach ($rowList as $row) {
-                $columnParams[ $row['Field'] ] = $row;
+                $columnParams[$row['Field']] = $row;
             }
             //END: get table columns params
-
             foreach ($entityParams['fields'] as $fieldName => $fieldParams) {
 
                 $columnName = \Espo\Core\Utils\Util::toUnderScore($fieldName);
@@ -100,7 +117,8 @@ class MysqlCharacter extends \Espo\Core\Services\Base
                     continue;
                 }
 
-                if (isset($fieldListExceededIndexMaxLength[$entityName]) && in_array($fieldName, $fieldListExceededIndexMaxLength[$entityName])) {
+                if (isset($fieldListExceededIndexMaxLength[$entityName])
+                    && in_array($fieldName, $fieldListExceededIndexMaxLength[$entityName])) {
                     continue;
                 }
 
@@ -115,8 +133,9 @@ class MysqlCharacter extends \Espo\Core\Services\Base
                     case 'text':
                     case 'jsonObject':
                     case 'jsonArray':
-                        $query = "ALTER TABLE `".$tableName."`
-                            CHANGE COLUMN `". $columnName ."` `". $columnName ."` ". $columnParams[$columnName]['Type'] ."
+                        $query
+                            = "ALTER TABLE `$tableName`
+                            CHANGE COLUMN `$columnName` `$columnName` " . $columnParams[$columnName]['Type'] . "
                             CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
                         ";
                         break;
@@ -129,20 +148,23 @@ class MysqlCharacter extends \Espo\Core\Services\Base
                         $sth = $pdo->prepare($query);
                         $sth->execute();
                     } catch (\Exception $e) {
-                        $GLOBALS['log']->warning('Utf8mb4: FAILED executing the query - [' . $query . '], details: '. $e->getMessage() .'.');
+                        $message = "Utf8mb4: FAILED executing the query - [$query], details: ";
+                        $message .= $e->getMessage() . '.';
+
+                        $GLOBALS['log']->warning($message);
                     }
                 }
             }
         }
 
-        $config = $container->get('config');
-        $database = $config->get('database');
+        $config = $this->getInjection('config');
+        $database = $this->getInjection('database');
         if (!isset($database['charset']) || $database['charset'] != 'utf8mb4') {
             $database['charset'] = 'utf8mb4';
             $config->set('database', $database);
             $config->save();
         }
 
-        $this->getContainer()->get('dataManager')->rebuild();
+        $this->getInjection('dataManager')->rebuild();
     }
 }
