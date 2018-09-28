b98992f89cf8d3ddde8e103a411df5ea4ee7e94f
r.ratsun <r.ratsun@zinitsolutions.com>
2018-09-28 14:02:51 +0300

    69349

--- application/Treo/Services/Email.php
+++ application/Treo/Services/Email.php
@@ -44,6 +44,37 @@ namespace Treo\Services;
 class Email extends \Espo\Services\Email
 {
     /**
+     * Send test email
+     *
+     * @param array $data
+     *
+     * @return bool
+     */
+    public function sendTestEmail($data)
+    {
+        $email = $this->getEntityManager()->getEntity('Email');
+        // get subject
+        $subject = $this
+            ->getEntityManager()
+            ->getContainer()
+            ->get('language')
+            ->translate('testEmailSubject', 'messages', 'Email');
+
+        $email->set(
+            [
+                'subject' => $subject,
+                'isHtml'  => false,
+                'to'      => $data['emailAddress']
+            ]
+        );
+
+        $emailSender = $this->getEntityManager()->getContainer()->get('mailSender');
+        $emailSender->useSmtp($data)->send($email);
+
+        return true;
+    }
+
+    /**
      * @inheritdoc
      */
     protected function init()
