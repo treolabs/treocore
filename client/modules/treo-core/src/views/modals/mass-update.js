11c1d02609d955333855527effdeb1ffa5b98b82
Valeriy Shamota <v.shamota@zinitsolutions.com>
2018-11-19 16:20:57 +0200

    set ids to model in modals/mass-update view

---
+++ client/modules/treo-core/src/views/modals/mass-update.js
@@ -0,0 +1,80 @@
+/*
+ * This file is part of EspoCRM and/or TreoPIM.
+ *
+ * EspoCRM - Open Source CRM application.
+ * Copyright (C) 2014-2018 Yuri Kuznetsov, Taras Machyshyn, Oleksiy Avramenko
+ * Website: http://www.espocrm.com
+ *
+ * TreoPIM is EspoCRM-based Open Source Product Information Management application.
+ * Copyright (C) 2017-2018 Zinit Solutions GmbH
+ * Website: http://www.treopim.com
+ *
+ * TreoPIM as well as EspoCRM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TreoPIM as well as EspoCRM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EspoCRM. If not, see http://www.gnu.org/licenses/.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "EspoCRM" word
+ * and "TreoPIM" word.
+ */
+
+Espo.define('treo-core:views/modals/mass-update', 'class-replace!treo-core:views/modals/mass-update', function (Dep) {
+
+    return Dep.extend({
+
+        setup: function () {
+            this.buttonList = [
+                {
+                    name: 'update',
+                    label: 'Update',
+                    style: 'danger',
+                    disabled: true
+                },
+                {
+                    name: 'cancel',
+                    label: 'Cancel'
+                }
+            ];
+
+            this.scope = this.options.scope;
+            this.ids = this.options.ids;
+            this.where = this.options.where;
+            this.selectData = this.options.selectData;
+            this.byWhere = this.options.byWhere;
+
+            this.header = this.translate(this.scope, 'scopeNamesPlural') + ' &raquo ' + this.translate('Mass Update');
+
+            this.wait(true);
+            this.getModelFactory().create(this.scope, function (model) {
+                this.model = model;
+                this.model.set({ids: this.ids});
+                this.getHelper().layoutManager.get(this.scope, 'massUpdate', function (layout) {
+                    layout = layout || [];
+                    this.fields = [];
+                    layout.forEach(function (field) {
+                        if (model.hasField(field)) {
+                            this.fields.push(field);
+                        }
+                    }, this);
+
+                    this.wait(false);
+                }.bind(this));
+            }.bind(this));
+
+            this.fieldList = [];
+        }
+    });
+});
