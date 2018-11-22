5aeb32455e06a5956ac0f7b174298d9aee74d2dc
Valeriy Shamota <v.shamota@zinitsolutions.com>
2018-11-22 15:52:04 +0200

    add wysiwyg show more text functional; change wysiwyg defs

--- client/modules/treo-core/src/views/fields/wysiwyg.js
+++ client/modules/treo-core/src/views/fields/wysiwyg.js
@@ -38,6 +38,37 @@ Espo.define('treo-core:views/fields/wysiwyg', 'class-replace!treo-core:views/fie
 
         detailTemplate: 'treo-core:fields/wysiwyg/detail',
 
+        detailMaxHeight: 400,
+
+        showMoreText: false,
+
+        showMoreDisabled: false,
+
+        events: {
+            'click a[data-action="seeMoreText"]': function (e) {
+                this.showMoreText = true;
+                this.reRender();
+            }
+        },
+
+        setup() {
+            Dep.prototype.setup.call(this);
+
+            this.detailMaxHeight = this.params.displayedHeight || this.detailMaxHeight;
+            this.showMoreDisabled = this.showMoreDisabled || this.params.showMoreDisabled;
+            this.showMoreText = false;
+        },
+
+        afterRender() {
+            Dep.prototype.afterRender.call(this);
+
+            if (this.mode === 'detail' || this.mode === 'list') {
+                if ((!this.model.has('isHtml') || this.model.get('isHtml')) && !this.showMoreText && !this.showMoreDisabled) {
+                    this.applyFieldPartHiding(this.name);
+                }
+            }
+        },
+
         fetch() {
             let data = Dep.prototype.fetch.call(this);
             return this.checkDataForDefaultTagsValue(data, this.name);
@@ -53,7 +84,45 @@ Espo.define('treo-core:views/fields/wysiwyg', 'class-replace!treo-core:views/fie
             }
 
             return data;
-        }
+        },
 
+        getValueForDisplay() {
+            let text = this.model.get(this.name);
+
+            if (this.mode === 'list' || (this.mode === 'detail' && (this.model.has('isHtml') && !this.model.get('isHtml')))) {
+                if (text && !this.showMoreText && !this.showMoreDisabled) {
+                    let isCut = false;
+
+                    if (text.length > this.detailMaxLength) {
+                        text = text.substr(0, this.detailMaxLength);
+                        isCut = true;
+                    }
+
+                    let nlCount = (text.match(/\n/g) || []).length;
+                    if (nlCount > this.detailMaxNewLineCount) {
+                        let a = text.split('\n').slice(0, this.detailMaxNewLineCount);
+                        text = a.join('\n');
+                        isCut = true;
+                    }
+
+                    if (isCut) {
+                        text += ' ...\n[#see-more-text]';
+                    }
+                }
+            }
+
+            return this.sanitizeHtml(text || '');
+        },
+
+        applyFieldPartHiding(name) {
+            let showMore = $(`<a href="javascript:" data-action="seeMoreText" data-name="${name}">${this.getLanguage().translate('See more')}</a>`);
+            if (!this.useIframe) {
+                let htmlContainer = this.$el.find(`.html-container[data-name="${name}"]`);
+                if (htmlContainer.height() > this.detailMaxHeight) {
+                    htmlContainer.parent().append(showMore);
+                    htmlContainer.css({maxHeight: this.detailMaxHeight + 'px', overflow: 'hidden', marginBottom: '10px'});
+                }
+            }
+        }
     })
 );
