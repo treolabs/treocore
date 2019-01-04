93e81fe6357223030ff33a5023675fed65935522
r.zablodskyi <r.zablodskiy@zinitsolutions.com>
2019-01-04 16:03:30 +0200

    added tests for Treo\Hooks\Portal\Hook

---
+++ tests/phpunit/application/Treo/Hooks/Portal/HookTest.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * This file is part of EspoCRM and/or TreoPIM.
+ *
+ * EspoCRM - Open Source CRM application.
+ * Copyright (C) 2014-2018 Yuri Kuznetsov, Taras Machyshyn, Oleksiy Avramenko
+ * Website: http://www.espocrm.com
+ *
+ * TreoPIM is EspoCRM-based Open Source Product Information Management application.
+ * Copyright (C) 2017-2018 TreoLabs GmbH
+ * Website: http://www.treopim.com
+ *
+ * TreoPIM as well as EspoCRM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TreoPIM as well as EspoCRM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EspoCRM. If not, see http://www.gnu.org/licenses/.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "EspoCRM" word
+ * and "TreoPIM" word.
+ */
+
+declare(strict_types=1);
+
+namespace Treo\Hooks\Portal;
+
+use Treo\PHPUnit\Framework\TestCase;
+
+/**
+ * Class HookTest
+ *
+ * @author r.zablodskiy@treolabs.com
+ */
+class HookTest extends TestCase
+{
+    /**
+     * Test is afterSave method exists
+     */
+    public function testIsAfterSaveMethodExists()
+    {
+        // test
+        $this->assertTrue(method_exists($this->createMockService(Hook::class, []), 'afterSave'));
+    }
+
+    /**
+     * Test is beforeSave method exists
+     */
+    public function testIsBeforeSaveMethodExists()
+    {
+        // test
+        $this->assertTrue(method_exists($this->createMockService(Hook::class, []), 'beforeSave'));
+    }
+
+    /**
+     * Test is afterRemove method exists
+     */
+    public function testisAfterRemoveMethodExists()
+    {
+        // test
+        $this->assertTrue(method_exists($this->createMockService(Hook::class, []), 'afterRemove'));
+    }
+}
