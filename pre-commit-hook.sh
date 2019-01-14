5420dae36e3beb42d88c95b2ba19e1a66cd8015a
r.ratsun <r.ratsun@zinitsolutions.com>
2019-01-14 12:13:29 +0200

    CI modifications

--- pre-commit-hook.sh
+++ pre-commit-hook.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # start PHP CodeSniffer
-/usr/bin/php tools/phpcs.phar -p --standard=PSR2 application/Treo/
+#/usr/bin/php tools/phpcs.phar -p --standard=PSR2 application/Treo/
 
 # start PHPUnit
 /usr/bin/php tools/phpunit.phar --bootstrap bootstrap.php tests
