c45048ab385c48fb550cc3d0d9e6c34b31239e7e
Valeriy Shamota <v.shamota@zinitsolutions.com>
2019-01-23 10:36:54 +0200

    72412
    added bool filters mechanic

---
+++ client/modules/treo-core/src/views/fields/filtered-link-multiple.js
@@ -0,0 +1,102 @@
+/*
+ * This file is part of EspoCRM and/or TreoPIM.
+ *
+ * EspoCRM - Open Source CRM application.
+ * Copyright (C) 2014-2018 Yuri Kuznetsov, Taras Machyshyn, Oleksiy Avramenko
+ * Website: http://www.espocrm.com
+ *
+ * TreoPIM is EspoCRM-based Open Source Product Information Management application.
+ * Copyright (C) 2017-2018 TreoLabs GmbH
+ * Website: http://www.treopim.com
+ *
+ * TreoPIM as well as EspoCRM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TreoPIM as well as EspoCRM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EspoCRM. If not, see http://www.gnu.org/licenses/.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "EspoCRM" word
+ * and "TreoPIM" word.
+ */
+
+Espo.define('treo-core:views/fields/filtered-link-multiple', 'views/fields/link-multiple',
+    Dep => Dep.extend({
+
+        selectBoolFilterList: [],
+
+        boolFilterData: {},
+
+        getBoolFilterData() {
+            let data = {};
+            this.selectBoolFilterList.forEach(item => {
+                if (typeof this.boolFilterData[item] === 'function') {
+                    data[item] = this.boolFilterData[item].call(this);
+                }
+            });
+            return data;
+        },
+
+        setup() {
+            Dep.prototype.setup.call(this);
+
+            if (this.mode != 'list') {
+                this.addActionHandler('selectLink', function () {
+                    this.notify('Loading...');
+
+                    let viewName = this.getMetadata().get('clientDefs.' + this.foreignScope + '.modalViews.select') || this.selectRecordsView;
+
+                    this.createView('dialog', viewName, {
+                        scope: this.foreignScope,
+                        createButton: !this.createDisabled && this.mode != 'search',
+                        filters: this.getSelectFilters(),
+                        boolFilterList: this.getSelectBoolFilterList(),
+                        boolFilterData: this.getBoolFilterData(),
+                        primaryFilterName: this.getSelectPrimaryFilterName(),
+                        createAttributes: (this.mode === 'edit') ? this.getCreateAttributes() : null,
+                        multiple: true
+                    }, view => {
+                        view.render();
+                        this.notify(false);
+                        this.listenToOnce(view, 'select', models => {
+                            this.clearView('dialog');
+                            if (Object.prototype.toString.call(models) !== '[object Array]') {
+                                models = [models];
+                            }
+                            models.forEach(model => {
+                                this.addLink(model.id, model.get('name'));
+                            });
+                        });
+                    });
+                });
+            }
+        },
+
+        getAutocompleteUrl() {
+            var url = Dep.prototype.getAutocompleteUrl.call(this);
+            var boolData = this.getBoolFilterData();
+            // add boolFilter data
+            if (boolData) {
+                url += '&' + $.param({'where':[{
+                        'type': 'bool',
+                        'data': boolData
+                    }]});
+            }
+
+            return url;
+        }
+
+    })
+);
+
