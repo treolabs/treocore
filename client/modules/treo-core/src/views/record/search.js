aa0303f187ca4e9e6e42b15a2dd3d86cff8014e4
Valeriy Shamota <v.shamota@zinitsolutions.com>
2019-01-23 15:01:12 +0200

    72412
    boolFilters

--- client/modules/treo-core/src/views/record/search.js
+++ client/modules/treo-core/src/views/record/search.js
@@ -39,10 +39,59 @@ Espo.define('treo-core:views/record/search', 'class-replace!treo-core:views/reco
 
         typesWithOneFilter: ['array', 'bool', 'enum', 'multiEnum', 'linkMultiple'],
 
+        hiddenBoolFilterList: [],
+
+        boolFilterData: [],
+
+        data() {
+            let data = Dep.prototype.data.call(this);
+            data.boolFilterListLength = 0;
+            data.boolFilterListComplex = data.boolFilterList.map(item => {
+                let includes = this.hiddenBoolFilterList.includes(item);
+                if (!includes) {
+                    data.boolFilterListLength++;
+                }
+                return {name: item, hidden: includes};
+            });
+            return data;
+        },
+
         setup: function () {
+            this.hiddenBoolFilterList = this.options.hiddenBoolFilterList || this.hiddenBoolFilterList;
+            this.boolFilterData = this.options.boolFilterData || this.boolFilterData;
             Dep.prototype.setup.call(this);
 
             _.extend(Dep.prototype.events, this.newEvents);
+        },
+
+        manageBoolFilters() {
+            (this.boolFilterList || []).forEach(item => {
+                if (this.bool[item] && !this.hiddenBoolFilterList.includes(item)) {
+                    this.currentFilterLabelList.push(this.translate(item, 'boolFilters', this.entityType));
+                }
+            });
+        },
+
+        updateCollection() {
+            this.collection.reset();
+            this.notify('Please wait...');
+            this.listenTo(this.collection, 'sync', function () {
+                this.notify(false);
+            }.bind(this));
+            let where = this.searchManager.getWhere();
+            where.forEach(item => {
+                if (item.type === 'bool') {
+                    let data = {};
+                    item.value.forEach(elem => {
+                        if (elem in this.boolFilterData) {
+                            data[elem] = this.boolFilterData[elem];
+                        }
+                    });
+                    item.data = data;
+                }
+            });
+            this.collection.where = where;
+            this.collection.fetch();
         },
 
         sortAdvanced: function (advanced) {
