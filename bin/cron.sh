be0220a24fc386a0c8438495347ba2fce51ee4c3
r.ratsun <r.ratsun@zinitsolutions.com>
2019-03-27 11:18:11 +0200

    refactoring

--- bin/cron.sh
+++ bin/cron.sh
@@ -21,20 +21,20 @@ $php console.php cron
 
 # self upgrade process
 if [[ ! "$(ps ax | grep treo-self-upgrade.sh)" =~ "bin/treo-self-upgrade.sh $id" ]]; then
-    bash ./bin/treo-self-upgrade.sh $id $php
+    setsid ./bin/treo-self-upgrade.sh $id $php >/dev/null 2>&1 &
 fi
 
 # module update process
 if [[ ! "$(ps ax | grep treo-module-update.sh)" =~ "bin/treo-module-update.sh $id" ]]; then
-    bash ./bin/treo-module-update.sh $id $php
+    setsid ./bin/treo-module-update.sh $id $php >/dev/null 2>&1 &
 fi
 
 # queue manager process
 if [[ ! "$(ps ax | grep treo-qm.sh)" =~ "bin/treo-qm.sh $id" ]]; then
-    bash ./bin/treo-qm.sh $id $php
+    setsid ./bin/treo-qm.sh $id $php >/dev/null 2>&1 &
 fi
 
 # notification process
 if [[ ! "$(ps ax | grep treo-notification.sh)" =~ "bin/treo-notification.sh $id" ]]; then
-    bash ./bin/treo-notification.sh $id $php
+    setsid ./bin/treo-notification.sh $id $php >/dev/null 2>&1 &
 fi
