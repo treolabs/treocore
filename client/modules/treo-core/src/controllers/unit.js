192138410b089694a25d0f57b7e830a388f728cb
Valeriy Shamota <v.shamota@zinitsolutions.com>
2019-04-08 16:09:33 +0300

    74744
    add unit controller

--- client/modules/treo-core/src/controllers/unit.js
+++ client/modules/treo-core/src/controllers/unit.js
@@ -31,6 +31,25 @@
  * and "TreoCore" word.
  */
 
-Espo.define('treo-core:controllers/settings', 'controllers/admin',
-    Dep => Dep.extend({})
-);
+Espo.define('treo-core:controllers/unit', 'controller', function (Dep) {
+
+    return Dep.extend({
+
+        defaultAction: "view",
+
+        view: function () {
+            var model = this.getConfig().clone();
+            model.defs = this.getConfig().defs;
+
+            model.once('sync', () => {
+                model.id = '1';
+                this.main('views/settings/edit', {
+                    model: model,
+                    headerTemplate: 'treo-core:admin/settings/headers/unit',
+                    recordView: 'treo-core:views/admin/unit'
+                });
+            });
+            model.fetch();
+        },
+    });
+});
