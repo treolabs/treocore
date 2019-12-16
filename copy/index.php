24ea924c33500a4b08995799924376584a4adcce
r.ratsun <r.ratsun@gmail.com>
2019-12-16 13:25:35 +0200

    79281

--- copy/index.php
+++ copy/index.php
@@ -1,10 +1,6 @@
 <?php
 /**
- * This file is part of EspoCRM and/or TreoCore.
- *
- * EspoCRM - Open Source CRM application.
- * Copyright (C) 2014-2019 Yuri Kuznetsov, Taras Machyshyn, Oleksiy Avramenko
- * Website: http://www.espocrm.com
+ * This file is part of TreoCore.
  *
  * TreoCore is EspoCRM-based Open Source application.
  * Copyright (C) 2017-2019 TreoLabs GmbH
