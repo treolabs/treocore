848e7f3303a072c3661908e1c6fbb412b51499af
Valeriy Shamota <v.shamota@zinitsolutions.com>
2020-02-04 18:07:28 +0200

    81479
    Fixed empty value;
    Added param for disabling empty option

--- client/modules/treo-core/src/views/fields/enum.js
+++ client/modules/treo-core/src/views/fields/enum.js
@@ -34,29 +34,33 @@
 Espo.define('treo-core:views/fields/enum', 'class-replace!treo-core:views/fields/enum',
     Dep => Dep.extend({
 
+        prohibitedEmptyValue: false,
+
         prohibitedScopes: ['Settings', 'EntityManager'],
 
         setup() {
             Dep.prototype.setup.call(this);
 
-            const scopeIsAllowed = !this.prohibitedScopes.includes(this.model.name);
-            const isArray = Array.isArray((this.params || {}).options);
+            this.prohibitedEmptyValue = this.prohibitedEmptyValue || this.options.prohibitedEmptyValue
+                || this.model.getFieldParam(this.name, 'prohibitedEmptyValue');
 
-            if (isArray && scopeIsAllowed && !this.params.options.includes('') && this.params.options.length > 1) {
-                this.params.options.unshift('');
+            if (!this.prohibitedEmptyValue) {
+                const scopeIsAllowed = !this.prohibitedScopes.includes(this.model.name);
+                const isArray = Array.isArray((this.params || {}).options);
 
-                if (Espo.Utils.isObject(this.translatedOptions)) {
-                    this.translatedOptions[''] = '';
+                if (isArray && scopeIsAllowed && !this.params.options.includes('') && this.params.options.length > 1) {
+                    this.params.options.unshift('');
+
+                    if (Espo.Utils.isObject(this.translatedOptions)) {
+                        this.translatedOptions[''] = '';
+                    }
+
+                    if (this.model.isNew() && this.mode === 'edit') {
+                        this.model.set({[this.name]: ''}, {silent: true});
+                    }
                 }
             }
-        },
-
-        afterRender() {
-            Dep.prototype.afterRender.call(this);
-
-            if (this.model.isNew() && this.mode === 'edit') {
-                this.model.set({[this.name]: ''}, { silent: true });
-            }
         }
+
     })
 );
